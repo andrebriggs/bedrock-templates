ae81b1e39c83b7b72e00f30549f41d857f0a4575
Andre Briggs <andrebriggs@users.noreply.github.com>
2020-05-09 18:39:51 -0700

    maldives

---
+++ build-strategy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ -z "$IMAGE_TAG" ]
+then
+    echo "No ImageTag provided"
+    export IMAGE_TAG=$(echo $(Build.BuildNumber) | tr / - | tr . - | tr _ - )
+else
+    echo "ImageTag is NOT empty"
+    export IMAGE_TAG=$(echo $IMAGE_TAG | tr / - | tr . - | tr _ - )
+fi
+export BUILD_REPO_NAME=$(echo $BUILD_REPO_NAME | tr '[:upper:]' '[:lower:]')
+export IMAGE_NAME=$BUILD_REPO_NAME:$IMAGE_TAG
+# echo ${{ parameters.ImageName }}
+# echo ${{ parameters.ImageTag }}
+echo $IMAGE_NAME
