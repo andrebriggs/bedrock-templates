7fb927762449706d652c6db79165041a168a701e
andrebriggs@users.noreply.github.com <andrebriggs@users.noreply.github.com>
2020-05-14 00:10:14 -0700

    spelling

--- scripts/azure-login.sh
+++ scripts/azure-login.sh
@@ -1,4 +1,4 @@
     set -e
 
-    echo "az login --service-principal --username $SP_APP_ID --tenant ${{ parameters.tenantId }}"
+    echo "az login --service-principal --username $SP_APP_ID --tenant $SP_TENANT"
     az login --service-principal --username "$SP_APP_ID" --password "$SP_PASS" --tenant "$SP_TENANT"
